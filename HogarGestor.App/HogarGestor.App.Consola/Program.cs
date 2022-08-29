87da66698606b8375e55c9f5784ffe845ea6db6d
Akami-Usagi <akami.3ds@gmail.com>
2022-08-29 11:01:15 -0500

    Clase Lunes 29 Agosto

--- HogarGestor.App/HogarGestor.App.Consola/Program.cs
+++ HogarGestor.App/HogarGestor.App.Consola/Program.cs
@@ -1,15 +1,19 @@
-﻿using System.Security.AccessControl;
+﻿
 using System;
 using HogarGestor.App.Dominio;
+using HogarGestor.App.Persistencia;
 
+namespace HogarGestor.App.Consola
+{
+    class program
+    {
 
-namespace HogarGestor.App.Consola{
-    class Program{
+        private static IRepositorioJoven _repoJoven = new RepositorioJoven(new Persistencia.AppContext());
         static void Main(string[] args){
-            
             Console.WriteLine("Hello, World!");
 
         }
+        
+
     }
 }
-
