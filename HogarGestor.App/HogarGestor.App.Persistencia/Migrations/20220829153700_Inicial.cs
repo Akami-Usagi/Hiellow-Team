d29b0114188f711ac55018684847521664c05140
Akami-Usagi <akami.3ds@gmail.com>
2022-08-29 10:39:40 -0500

    cambios en la migracion

---
+++ HogarGestor.App/HogarGestor.App.Persistencia/Migrations/20220829153700_Inicial.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HogarGestor.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Historias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Registro = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Historias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatronesCrecimientoJoven",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Medicion = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    HistoriaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatronesCrecimientoJoven", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatronesCrecimientoJoven_Historias_HistoriaId",
+                        column: x => x.HistoriaId,
+                        principalTable: "Historias",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Documento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genero = table.Column<int>(type: "int", nullable: false),
+                    DocumentoTipo = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Parentesco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CorreoElectronico = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Latitud = table.Column<float>(type: "real", nullable: true),
+                    longitud = table.Column<float>(type: "real", nullable: true),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FamiliarDesignadoId = table.Column<int>(type: "int", nullable: true),
+                    PediatraId = table.Column<int>(type: "int", nullable: true),
+                    NutricionistaId = table.Column<int>(type: "int", nullable: true),
+                    HistoriaJovenId = table.Column<int>(type: "int", nullable: true),
+                    EspecialidadMedica = table.Column<int>(type: "int", nullable: true),
+                    Rethus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Personas_Historias_HistoriaJovenId",
+                        column: x => x.HistoriaJovenId,
+                        principalTable: "Historias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Personas_Personas_FamiliarDesignadoId",
+                        column: x => x.FamiliarDesignadoId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Personas_Personas_NutricionistaId",
+                        column: x => x.NutricionistaId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Personas_Personas_PediatraId",
+                        column: x => x.PediatraId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SugerenciasCuidado",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HistoriaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SugerenciasCuidado", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SugerenciasCuidado_Historias_HistoriaId",
+                        column: x => x.HistoriaId,
+                        principalTable: "Historias",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatronesCrecimientoJoven_HistoriaId",
+                table: "PatronesCrecimientoJoven",
+                column: "HistoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_FamiliarDesignadoId",
+                table: "Personas",
+                column: "FamiliarDesignadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_HistoriaJovenId",
+                table: "Personas",
+                column: "HistoriaJovenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_NutricionistaId",
+                table: "Personas",
+                column: "NutricionistaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_PediatraId",
+                table: "Personas",
+                column: "PediatraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SugerenciasCuidado_HistoriaId",
+                table: "SugerenciasCuidado",
+                column: "HistoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatronesCrecimientoJoven");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "SugerenciasCuidado");
+
+            migrationBuilder.DropTable(
+                name: "Historias");
+        }
+    }
+}
