78b1f33a001b208de3b477bc7ff58b0bfaec1c8c
Akami-Usagi <akami.3ds@gmail.com>
2022-08-31 09:39:07 -0500

    pre edicion

--- HogarGestor.App/HogarGestor.App.Consola/Program.cs
+++ HogarGestor.App/HogarGestor.App.Consola/Program.cs
@@ -10,9 +10,25 @@ namespace HogarGestor.App.Consola
         private static IRepositorioJoven _repoJoven = new RepositorioJoven(new Persistencia.AppContext());
         static void Main(string[] args){
             Console.WriteLine("Hello, World!");
+            AddJoven();
 
         }
         
+        private static void AddJoven(){
+            var joven = new Joven{
+                Nombre = "Camilo",
+                Apellidos = "Arango Escobar",
+                NumeroTelefono = "3103565058",
+                Documento = "1115066671",
+                Genero = Generos.Masculino,
+                Latitud =  5.07F,
+                longitud = -75.687F,
+                Ciudad = "Buga",
+                FechaNacimiento = new DateTime(1987,06,07)
+
+            };
+            _repoJoven.AddJoven(joven);
+        }
 
     }
 }
